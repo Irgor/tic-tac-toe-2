821fc72849801b2c0ffa2fa014e37efc654e59d8
Igor <igornascimento02@gmail.com>
2023-09-24 02:46:32 -0300

    add url to api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,8 @@ import { FieldComponent } from './components/field/field.component';
 import { HomeComponent } from './pages/home/home.component';
 
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
-// const config: SocketIoConfig = { url: 'wss://tic-tac-toe-2-api.onrender.com', options: {} };
-const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
+const config: SocketIoConfig = { url: 'wss://tic-tac-toe-2-api.onrender.com', options: {} };
+// const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
 
 @NgModule({
   declarations: [
